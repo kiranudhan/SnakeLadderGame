41ddff7065041c5533a724bf28e8be854d171a81
kiranudhan <udhankiran62@gmail.com>
2023-03-17 17:07:29 +0530

    UC2 -> get a number between 1 to 6 using random function

--- src/com/SnakeLadderMain.java
+++ src/com/SnakeLadderMain.java
@@ -1,10 +1,15 @@
 package com;
 
+import java.util.Random;
+
 public class SnakeLadderMain {
     public static void main(String[] args) {
         System.out.println("Welcome to Snake and Ladder");
         int StartPos = 0;
         System.out.println("Game Starts Now");
         System.out.println("Starting position of player is: "+StartPos);
+        Random rd = new Random();
+        int diceroll = rd.nextInt(7);//0-6
+        System.out.println("Player gets Dice Number :: " + diceroll);
     }
 }
